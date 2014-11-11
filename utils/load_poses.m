80a6a633bc9bbbff4909d306771293b8ca7f7cb0
Vincent Delaitre <vincent.delaitre@gmail.com>
2014-11-11 16:21:40 +0100

    load poses

--- utils/load_poses.m
+++ utils/load_poses.m
@@ -1,3 +1,13 @@
 function [cleanbb detIDScores negbb] = load_poses(params, vid)
-    load(fullfile(params.root, 'tmp', vid.id, sprintf('%s_cleanbb.mat', params.detector_prefix)), 'cleanbb', 'negbb', 'detIDScores');      
+    load(fullfile(params.root, 'tmp', vid, sprintf('%s_cleanbb.mat', params.detector_prefix)), 'cleanbb', 'negbb', 'detIDScores');      
+    
+    th = params.det_th;
+    for i = 1 : length(cleanbb)
+        if ~isempty(cleanbb{i})
+            scoreMin = th(cleanbb{i}(:, end - 1))';
+            cleanbb{i} = cleanbb{i}(cleanbb{i}(:, end) >= scoreMin, :);
+        else
+            cleanbb{i} = [];  % get rid of the 0xn matrices
+        end
+    end
 end
